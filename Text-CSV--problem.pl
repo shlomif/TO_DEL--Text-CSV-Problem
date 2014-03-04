c7eeb93143a171d9f6b37e1c06b7ace468740b07
Shlomi Fish <shlomif@shlomifish.org>
2014-03-04 15:47:30 +0200

    Still reproducing with Text::CSV_PP.
    And an explicit line loop.

--- Text-CSV--problem.pl
+++ Text-CSV--problem.pl
@@ -3,22 +3,23 @@
 use warnings;
 use strict;
 
-use Text::CSV;
+use Text::CSV_PP;
 use Data::Dumper qw(Dumper);
 
 my $fn = 'test.csv';
 open my $IF, "<", $fn or die "Could not open input file: $!";
 
-my $csv = Text::CSV->new({
+my $csv = Text::CSV_PP->new({
   allow_whitespace    => 1,
   allow_loose_escapes => 1,
 }) or die "Cannot use CSV: ".Text::CSV->error_diag();
 
 $csv->column_names( $csv->getline($IF) );
 
-my $data;
+my @lines;
 while ( my $line = $csv->getline_hr($IF) ) {
-  $data->{ $line->{'CUSTOMER CODE'} } = $line;
+    push @lines, +{ %{$line} };
+    # $data->{ $line->{'CUSTOMER CODE'} } = $line;
 }
 
-print Dumper([$data]);
+print Dumper([\@lines]);
